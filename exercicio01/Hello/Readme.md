eb820d96dd68a90a6ce23474fb9580ac76ad9821
Welfemberg <welfemberg.diniz@hotmail.com>
2025-12-27 00:37:41 -0300

    Dia 01: Hello World em Java

---
+++ exercicio01/Hello/Readme.md
@@ -0,0 +1,15 @@
+# Dia 01 - Hello World em Java
+
+Exercício introdutório em Java.
+
+## Objetivo
+- Entender a estrutura básica de um programa Java
+- Utilizar o método main
+- Exibir mensagens no console
+
+## Conceitos utilizados
+- Classe
+- Método main
+- String
+- System.out.println
+- System.out.printf
---
+++ exercicio01/Hello/src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] Args){
+       System.out.println("Hello World!");
+       String nome = "Berg";
+
+       // O %s será substituido pela variável que declarei no final. Neste caso, a variável: nome.
+        // Para que dê certo, utilizo o printf ao invés de println ou print.
+       System.out.printf("Olá! Me chamo %s e estou iniciando meus estudos em Java. \n", nome);
+
+    }
+}
